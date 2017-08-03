9ca1a23a10d2d86e077f17b710add221e77fd9e5
Jing Wei Li <ljw9801055@gmail.com>
2017-08-02 22:40:43 -0400

    basic operations working

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -4,22 +4,96 @@
 //
 //  Created by Jing Wei Li on 7/30/17.
 //  Copyright © 2017 Jing Wei Li. All rights reserved.
-//
+//~
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var isTyping = false;
+    var isEmpty = true;
+    var computedVal:Double = 0
+    var result:Double  = 0
+    var mode = ""
+    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        } set {
+            display.text = String(newValue)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func PressNum(_ sender: UIButton) {
+        let curr = sender.currentTitle!
+        
+        
+        if isTyping {
+            let val = display.text!
+            display.text = val + curr
+        } else {
+            display!.text = curr
+            isTyping = true
+        }
+        computedVal = displayValue
     }
+    
+    @IBAction func PerformOperations(_ sender: UIButton) {
+        let toCompute = sender.currentTitle!
+        
+        switch toCompute {
+        case "+":
+            result += computedVal
+            mode = "+"
+        case "-":
+            if result == 0{
+                result = computedVal
+            } else {
+                result -= computedVal
+            }
+            mode = "-"
+        case "*":
+            if result == 0{
+                result = computedVal
+            } else {
+                result *= computedVal
+            }
+            mode = "*"
+        case "/":
+            if result == 0{
+                result = computedVal
+            } else {
+                result /= computedVal
+            }
+            mode = "/"
+        case "=":
+            switch mode {
+            case "+":
+                result += computedVal
+            case "-":
+                result -= computedVal
+            case "*":
+                result *= computedVal
+            case "/":
+                result /= computedVal
+            default:
+                break
+            }
+            displayValue = result
+            result = 0
+        case "C":
+            displayValue = 0
+            result = 0
+        default:
+            break
+        }
+        isTyping = false;
 
+        
+    }
+    
 
 }
-
